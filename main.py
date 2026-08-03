a266fe895ed51fdd4855e50294e971b59239a4e5
Isabella Bryce-Smith Flores <isabellabrycesf@gmail.com>
2026-08-03 15:46:07 +0100

    Added mission data and summary

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
+# 🚀 Mission Control Dashboard
+
+missions = [
+    {
+        "name": "Artemis II",
+        "destination": "Moon",
+        "crew": 4,
+        "status": "In preparation"
+    },
+    {
+        "name": "Europa Clipper",
+        "destination": "Jupiter",
+        "crew": 0,
+        "status": "Active"
+    },
+    {
+        "name": "Mars Sample Return",
+        "destination": "Mars",
+        "crew": 0,
+        "status": "Planning"
+    }
+]
+
 print("================================")
 print("       🚀 MISSION CONTROL")
 print("================================")
-
 print()
-print("Welcome to Mission Control!!")
-print("Your dashboard is starting...")
+
+for mission in missions:
+    print(f"Mission: {mission['name']}")
+    print(f"Destination: {mission['destination']}")
+    print(f"Crew: {mission['crew']}")
+    print(f"Status: {mission['status']}")
+    print("--------------------------------")
+    print()
+print("MISSION SUMMARY")
+print("--------------------------------")
+
+total_missions = len(missions)
+active_missions = 0
+
+for mission in missions:
+    if mission["status"] == "Active":
+        active_missions += 1
+
+print(f"Total missions: {total_missions}")
+print(f"Active missions: {active_missions}")
